ccf11ba74850af72a3e4a866ec821b03e710fe38
Divenero <artemmartus2012@gmail.com>
2019-10-30 09:50:52 +0200

    Initial Commit

---
+++ SaveTheMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SaveTheMap
+//
+//  Created by Artem Martus on 30.10.2019.
+//  Copyright © 2019 Artem Martus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
